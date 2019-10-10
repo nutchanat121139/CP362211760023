48bb644ab928b54ef16fde84b6b406a0f73cd4cc
Nutchanat <nutchanataz4477@gmail.com>
2019-10-10 14:10:41 +0700

    ex8

---
+++ src/lab7/ex8.java
@@ -0,0 +1,77 @@
+package lab7;
+
+public class ex8 {
+    public static void main(String[] args) {
+
+        for (int i = 8; i < =1 ; i--) {
+            for (int j=i; j>=i ; j-- ) {
+                System.out.print("*");
+            }//for j
+            System.out.print();
+            }//for i
+
+
+        System.out.println("\n\n");
+
+        for (int i = 1; i < =8 ; i++) {
+            for (int j=i; j<=i ; j++ ) {
+                System.out.print(j+" ");
+            }//for j
+            System.out.print();
+            System.out.println();
+        }//for i
+
+        for (int i = 8; i < =1 ; i--) {
+            for (int j=i; j>=i ; j-- ) {
+                System.out.print("*");
+            }//for j
+            System.out.print();
+        }//for i
+
+
+        System.out.println("\n\n");
+
+        int count = i;
+        for (int i = 1; i < =8 ; i++) {
+            for (int j=i; j<=i ; j++ ) {
+                System.out.print(count+" ");
+                count++;
+            }//for j
+            System.out.println();
+        }//for i
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //main
+
+
+//class
+    }
+}
